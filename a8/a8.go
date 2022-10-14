0f2d99666c4d03563b01d27224e92f468effbb92
xiaoxinmiao <xiaoxm_001@163.com>
2022-10-15 04:19:57 +0800

    v2.0.0

--- a8/a8.go
+++ a8/a8.go
@@ -1,5 +1,5 @@
 package a8
 
 func Hello() string {
-	return "hello a8 v1.1.1"
+	return "hello a8 v2.0.0"
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/relax-space/go-a8
+module github.com/relax-space/go-a8/v2
 
 go 1.19
